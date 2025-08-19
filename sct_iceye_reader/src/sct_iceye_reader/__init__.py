42fd76538393455a98233b71bb5358dfd6468cc7
Giorgio Parma <giorgio.parma@aresys.it>
2025-08-19 09:46:58 +0200

    iceye: changed plugin interface according to latest SCT plugins protocol for ALE corrections

--- sct_iceye_reader/src/sct_iceye_reader/__init__.py
+++ sct_iceye_reader/src/sct_iceye_reader/__init__.py
@@ -6,8 +6,8 @@ SCT-Plugin: ICEYE product format reader
 ---------------------------------------
 """
 
-from sct_iceye_reader.interface import get_azimuth_corrections, get_detector, get_manager, get_range_corrections
+from sct_iceye_reader.interface import get_ale_corrector, get_detector, get_manager
 
-__all__ = ["get_manager", "get_detector", "get_azimuth_corrections", "get_range_corrections"]
+__all__ = ["get_manager", "get_detector", "get_ale_corrector"]
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
