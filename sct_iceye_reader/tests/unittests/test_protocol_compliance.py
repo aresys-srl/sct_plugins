613951bb3d1eef0f22aec4ce56c5d812cbb35df7
Giorgio Parma <giorgio.parma@aresys.it>
2025-08-19 09:48:10 +0200

    iceye: adding unittest for testing protocol compliance

---
+++ sct_iceye_reader/tests/unittests/test_protocol_compliance.py
@@ -0,0 +1,62 @@
+# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
+# SPDX-License-Identifier: MIT
+
+"""
+SCT ICEYE Product Format Reader Plugin - Testing Plugin Protocol Compliance
+"""
+
+from __future__ import annotations
+
+import unittest
+
+from arepyextras.eo_products.iceye.l1_products.utilities import is_iceye_product
+from arepyextras.quality.io.quality_input_protocol import ChannelData, SARCoordinatesFunction
+from sct.io.extended_protocols import SCTInputProduct
+from sct.io.input_product_plugins import import_input_product_plugins
+
+from sct_iceye_reader.protocol_implementation import (
+    ICEYEChannelManager,
+    ICEYEDopplerPolynomial,
+    ICEYEProductManager,
+)
+
+
+class PluginProtocolComplianceTest(unittest.TestCase):
+    """Test Plugin Protocol Compliance"""
+
+    def setUp(self):
+        self.plugin = import_input_product_plugins([])
+
+    def test_installed_plugin(self) -> None:
+        """Testing correct plugin installation"""
+        self.assertEqual(len(self.plugin), 1)
+        self.assertEqual(self.plugin[0].__name__, "sct_iceye_reader")
+
+    def test_get_manager(self) -> None:
+        """Testing manager protocol compliance"""
+        isinstance(self.plugin[0].get_manager(), ICEYEProductManager)
+        isinstance(self.plugin[0].get_manager(), SCTInputProduct)
+
+    def test_get_detector(self) -> None:
+        """Testing detector protocol compliance"""
+        self.assertTrue(self.plugin[0].get_detector() is is_iceye_product)
+
+    def test_get_ale_corrector(self) -> None:
+        """Testing ALE Corrector protocol compliance"""
+        self.assertIsNone(self.plugin[0].get_ale_corrector())
+
+    def test_product_protocol_compliance(self) -> None:
+        """Testing product protocol implementation compliance"""
+        isinstance(ICEYEProductManager, SCTInputProduct)
+
+    def test_channel_protocol_compliance(self) -> None:
+        """Testing channel protocol implementation compliance"""
+        isinstance(ICEYEChannelManager, ChannelData)
+
+    def test_polynomial_protocol_compliance(self) -> None:
+        """Testing polynomial protocol implementation compliance"""
+        isinstance(ICEYEDopplerPolynomial, SARCoordinatesFunction)
+
+
+if __name__ == "__main__":
+    unittest.main()
