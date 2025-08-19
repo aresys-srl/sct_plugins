0d97aa66a96539b7e827925a19dc112e5a8b243c
Giorgio Parma <giorgio.parma@aresys.it>
2025-08-19 10:44:47 +0200

    eos04: changed plugin interface according to latest SCT plugins protocol for ALE corrections

--- sct_eos04_reader/src/sct_eos04_reader/__init__.py
+++ sct_eos04_reader/src/sct_eos04_reader/__init__.py
@@ -6,8 +6,8 @@ SCT-Plugin: EOS-04 product format reader
 ----------------------------------------
 """
 
-from sct_eos04_reader.interface import get_azimuth_corrections, get_detector, get_manager, get_range_corrections
+from sct_eos04_reader.interface import get_ale_corrector, get_detector, get_manager
 
-__all__ = ["get_manager", "get_detector", "get_azimuth_corrections", "get_range_corrections"]
+__all__ = ["get_manager", "get_detector", "get_ale_corrector"]
 
 __version__ = "1.0.0"
