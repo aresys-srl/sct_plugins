23f5a03a2d0512ce215e56834bfbc6640d1b4faf
Giorgio Parma <giorgio.parma@aresys.it>
2026-03-05 10:32:20 +0100

    iceye: update plugin to match latest SCT > 2.1 updates (stevedore plugin)

--- sct_iceye_reader/src/sct_iceye_reader/interface.py
+++ sct_iceye_reader/src/sct_iceye_reader/interface.py
@@ -3,29 +3,37 @@
 
 """
 ICEYE format PERSEO-Quality protocol-compliant wrapper
------------------------------------------------------------
+------------------------------------------------------
 """
 
 from __future__ import annotations
 
 from pathlib import Path
-from typing import Callable
+from typing import TYPE_CHECKING, Callable
 
-from eo_products.iceye.utilities import is_iceye_product
+from sct_iceye_reader import __version__
 
-from sct_iceye_reader.protocol_implementation import ICEYEProductManager
-
-
-def get_manager() -> type[ICEYEProductManager]:
-    """Retrieve manager"""
-    return ICEYEProductManager
+if TYPE_CHECKING:
+    from sct.io.extended_protocols import ALECorrectionFunctionType, SCTInputProduct
 
 
-def get_detector() -> Callable[[str | Path], bool]:
-    """Retrieve detector"""
-    return is_iceye_product
+class ICEYEProductPlugin:
+    """Plugin for ICEYE product format"""
 
+    version = __version__
 
-def get_ale_corrector() -> None:
-    """Retrieve ALE corrector class"""
-    return None
+    @classmethod
+    def get_manager(cls) -> type[SCTInputProduct]:
+        from sct_iceye_reader.protocol_implementation import ICEYEProductManager
+
+        return ICEYEProductManager
+
+    @classmethod
+    def get_detector(cls) -> Callable[[str | Path], bool]:
+        from eo_products.iceye.utilities import is_iceye_product
+
+        return is_iceye_product
+
+    @classmethod
+    def get_ale_corrector(cls) -> ALECorrectionFunctionType:
+        return None
