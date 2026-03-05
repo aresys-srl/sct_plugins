2e57a2081cec72fb0de6ed6e9e66e33eabbfbfbe
Giorgio Parma <giorgio.parma@aresys.it>
2026-03-05 11:05:36 +0100

    eos04: update plugin to match latest SCT > 2.1 updates (stevedore plugin)

--- sct_eos04_reader/docs/source/install.rst
+++ sct_eos04_reader/docs/source/install.rst
@@ -4,7 +4,7 @@ Installation
 Requirements
 ------------
 
-SCT EOS-04 Reader plugin requires a Python version **equal or higher than 3.10**.
+SCT EOS-04 Reader plugin requires a Python version **equal or higher than 3.11**.
 
 
 Install with pip
--- sct_eos04_reader/src/sct_eos04_reader/__init__.py
+++ sct_eos04_reader/src/sct_eos04_reader/__init__.py
@@ -6,8 +6,4 @@ SCT-Plugin: EOS-04 product format reader
 ----------------------------------------
 """
 
-from sct_eos04_reader.interface import get_ale_corrector, get_detector, get_manager
-
-__all__ = ["get_manager", "get_detector", "get_ale_corrector"]
-
 __version__ = "1.0.0"
