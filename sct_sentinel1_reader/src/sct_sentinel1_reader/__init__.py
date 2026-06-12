05c4b1250015fc37774654fd285cdaebb0a6983b
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-12 15:55:14 +0200

    sentinel1: version bump

--- sct_sentinel1_reader/src/sct_sentinel1_reader/__init__.py
+++ sct_sentinel1_reader/src/sct_sentinel1_reader/__init__.py
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
 # SPDX-License-Identifier: MIT
 
-"""
-SCT-Plugin: Sentinel-1 product format reader
---------------------------------------------
-"""
+"""SCT-Plugin: Sentinel-1 product format reader"""
 
-__version__ = "1.1.9"
+__version__ = "1.2.0"
