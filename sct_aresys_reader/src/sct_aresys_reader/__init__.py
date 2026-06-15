4954cf165f29d3f00649fd835bcdad6b59d5bc05
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-15 08:55:03 +0200

    aresys: version bump

--- sct_aresys_reader/src/sct_aresys_reader/__init__.py
+++ sct_aresys_reader/src/sct_aresys_reader/__init__.py
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
 # SPDX-License-Identifier: MIT
 
-"""
-SCT-Plugin: Aresys product format reader
-----------------------------------------
-"""
+"""SCT-Plugin: Aresys product format reader."""
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"
