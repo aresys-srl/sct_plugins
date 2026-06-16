d46329ba00348d2af139a4f0251314a21357a450
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-16 09:42:17 +0200

    novasar1: switching from unittest to pytest

--- sct_novasar1_reader/noxfile.py
+++ sct_novasar1_reader/noxfile.py
@@ -1,12 +1,9 @@
 # SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
 # SPDX-License-Identifier: MIT
 
-"""Automating python testing, formatting and distribution of SCT NovaSAR1 Plugin"""
+"""Automating python testing, formatting and distribution of SCT NovaSAR-1 Plugin"""
 
 import sys
-from pathlib import Path
-
-import nox
 
 sys.path.append("..")
 import nox_common
