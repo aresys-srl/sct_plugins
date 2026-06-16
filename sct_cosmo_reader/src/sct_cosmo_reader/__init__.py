e302874112252d99298ae096008a6500f8a92794
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-16 16:23:50 +0200

    cosmo: version bump

--- sct_cosmo_reader/src/sct_cosmo_reader/__init__.py
+++ sct_cosmo_reader/src/sct_cosmo_reader/__init__.py
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
 # SPDX-License-Identifier: MIT
 
-"""
-SCT-Plugin: COSMO-SkyMed product format reader
-----------------------------------------------
-"""
+"""SCT-Plugin: COSMO-SkyMed product format reader."""
 
-__version__ = "1.1.2"
+__version__ = "1.2.0"
