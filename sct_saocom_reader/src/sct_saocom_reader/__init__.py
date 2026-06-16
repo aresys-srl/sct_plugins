520e9ca5e3d9c8ebc6c26647809c73f6b02a99e9
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-16 15:11:08 +0200

    saocom: version bump

--- sct_saocom_reader/src/sct_saocom_reader/__init__.py
+++ sct_saocom_reader/src/sct_saocom_reader/__init__.py
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
 # SPDX-License-Identifier: MIT
 
-"""
-SCT-Plugin: Saocom product format reader
----------------------------------------
-"""
+"""SCT-Plugin: Saocom product format reader."""
 
-__version__ = "1.1.2"
+__version__ = "1.2.0"
