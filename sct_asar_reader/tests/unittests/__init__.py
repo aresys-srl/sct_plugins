521cf2f32827d6240ea757d24ae85c52bd4b0e40
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-17 10:22:08 +0200

    asar: switching from unittest to pytest

--- sct_asar_reader/tests/unittests/__init__.py
+++ sct_asar_reader/tests/unittests/__init__.py
@@ -1,6 +1,4 @@
 # SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
 # SPDX-License-Identifier: MIT
 
-"""
-SCT ASAR Product Format Reader Plugin - Unittest
-"""
+"""SCT ASAR Product Format Reader Plugin - Unittest."""
