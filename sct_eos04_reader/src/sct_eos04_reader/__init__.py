53dd66cb6dd43a84c82e3455461452dd9fa277fb
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-17 08:44:01 +0200

    eos04: version bump

--- sct_eos04_reader/src/sct_eos04_reader/__init__.py
+++ sct_eos04_reader/src/sct_eos04_reader/__init__.py
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
 # SPDX-License-Identifier: MIT
 
-"""
-SCT-Plugin: EOS-04 product format reader
-----------------------------------------
-"""
+"""SCT-Plugin: EOS-04 product format reader."""
 
-__version__ = "1.0.0"
+__version__ = "1.2.0"
