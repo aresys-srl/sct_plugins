c01985f3240170e8cc0896f62803c844daa6119f
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-17 09:44:35 +0200

    strix: switching from unittest to pytest

--- sct_strix_reader/tests/__init__.py
+++ sct_strix_reader/tests/__init__.py
@@ -1,6 +1,4 @@
 # SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
 # SPDX-License-Identifier: MIT
 
-"""
-SCT StriX Product Format Reader Plugin - Tests
-"""
+"""SCT StriX Product Format Reader Plugin - Tests."""
