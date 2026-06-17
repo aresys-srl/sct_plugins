d01146822f0ff8efdd0ef1ddd138fa7b2f674790
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-17 09:38:33 +0200

    strix: version bump

--- sct_strix_reader/src/sct_strix_reader/__init__.py
+++ sct_strix_reader/src/sct_strix_reader/__init__.py
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
 # SPDX-License-Identifier: MIT
 
-"""
-SCT-Plugin: Synspective StriX product format reader
----------------------------------------------------
-"""
+"""SCT-Plugin: Synspective StriX product format reader."""
 
-__version__ = "1.0.0"
+__version__ = "1.2.0"
