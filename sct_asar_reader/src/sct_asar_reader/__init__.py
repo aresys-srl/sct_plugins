dc7ca7835c6532b75a1d17d3de876c354f4655f0
Giorgio Parma <giorgio.parma@aresys.it>
2026-06-17 10:11:21 +0200

    asar: version bump

--- sct_asar_reader/src/sct_asar_reader/__init__.py
+++ sct_asar_reader/src/sct_asar_reader/__init__.py
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
 # SPDX-License-Identifier: GPLv3+
 
-"""
-SCT-Plugin: ENVISAT/ERS product in ASAR format reader
------------------------------------------------------
-"""
+"""SCT-Plugin: ENVISAT/ERS product in ASAR format reader."""
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"
