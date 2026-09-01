02ce1a54677e6bfb9073076fd4c336563f3edaaa
Giorgio Parma <giorgio.parma@aresys.it>
2026-09-01 16:00:28 +0200

    removing nox from the whole project

--- sct_strix_reader/noxfile.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: Aresys S.r.l. <info@aresys.it>
-# SPDX-License-Identifier: MIT
-
-"""Automating python testing, formatting and distribution of SCT StriX Plugin"""
-
-import sys
-
-sys.path.append("..")
-import nox_common
